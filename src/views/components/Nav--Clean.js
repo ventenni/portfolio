3f1f28330c2ade52373dc50fca3b8a7e164b6f16
ventenni <ventenni@gmail.com>
2020-05-21 11:46:06 +1000

    tidied up clean nav and added collapse for mobile

--- src/views/components/Nav--Clean.js
+++ src/views/components/Nav--Clean.js
@@ -1,38 +1,48 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import '../../assets/styles/components/nav--clean.scss';
 
 // Reactstrap Components
 import {
-  Navbar,
-  NavbarBrand,
-  Nav,
-  NavItem,
-  NavLink
+	Collapse,
+	Navbar,
+	NavbarBrand,
+	NavbarToggler,
+	Nav,
+	NavItem,
+	NavLink
 } from 'reactstrap';
 
 const SiteNav = (props) => { 
+	const [isOpen, setIsOpen] = useState(false);
+
+	const toggleNavbar = () => setIsOpen(!isOpen);
+	
 	return (
-		<Navbar light className="nav-clean">
-			<Nav navbar>
-				<div className="navbar-left-side">
+		<Navbar light className="nav-clean" expand="md">
+			
+				<div className="nav-clean__left">
 					<NavbarBrand>
-						<img src="http://via.placeholder.com/100" height="100px" width="100px" alt="Logo" />
+						<img src="http://via.placeholder.com/100" height="50px" width="100px" alt="Logo" />
 					</NavbarBrand>
 				</div>
 
-				<div className="navbar-right-side">
-					<NavItem>
-						<NavLink href="#">Link</NavLink>
-					</NavItem>
-					<NavItem>
-						<NavLink href="#">Link</NavLink>
-					</NavItem>
-					<NavItem>
-						<NavLink href="#">Link</NavLink>
-					</NavItem>
-				</div>
-			</Nav>
+			<div className="nav-clean__right">
+				<Collapse isOpen={isOpen} navbar>
+					<Nav>
+						<NavItem>
+							<NavLink href="#">Link</NavLink>
+						</NavItem>
+						<NavItem>
+							<NavLink href="#">Link</NavLink>
+						</NavItem>
+						<NavItem>
+							<NavLink href="#">Link</NavLink>
+						</NavItem>
+					</Nav>
+				</Collapse>
+				<NavbarToggler onClick={toggleNavbar} className="mr-2" />
+			</div>
 		</Navbar>
 	)
 }

4143ffc51159092eeac0ae43b8b8fcc0518e701f
ventenni <ventenni@gmail.com>
2020-05-30 14:27:17 +1000

    added button that alerts with coords

--- src/views/components/ActivityItem.js
+++ src/views/components/ActivityItem.js
@@ -1,5 +1,9 @@
 import React from 'react';
 
+// Libraries
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faMapMarker } from '@fortawesome/free-solid-svg-icons';
+
 // Style
 import '../../assets/styles/components/activity-item.scss';
 
@@ -7,18 +11,28 @@ const ActivityItem = ({activity, location, date}) => {
 	return (
 		<div className="activity-item">
 
-			<p className="activity-item__description">{activity}</p>
+			<div className="activity-item__text-items">
 
-			<p className="activity-item__location">
-				{`${location.address}`}
-				<br />
-				{`${location.city}, ${location.country}`}
-			</p>
+				<p className="activity-item__text-items__description">{activity}</p>
 
-			<p className="activity-item__date">{date}</p>
+				<p className="activity-item__text-items__location">
+					{`${location.address}`}
+					<br />
+					{`${location.city}, ${location.country}`}
+				</p>
+
+				<p className="activity-item__text-items__date">{date}</p>
+
+			</div>
+
+			<div className="activity-item__button">
+				<button className="btn" onClick={() => alert(`${location.latitude}, ${location.longitude}`)}>
+					<FontAwesomeIcon icon={faMapMarker} size="2x" />
+				</button>
+			</div>
 			
 			<hr />
-			
+
 		</div>
 	);
 }

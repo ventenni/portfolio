ee1910dd7d0ac21ce2e2e5f82ec7942dc01cfd2c
ventenni <ventenni@gmail.com>
2020-07-23 17:27:41 +1000

    added api key to json file and added to git ignore

--- src/views/components/YouTubeSearch.js
+++ src/views/components/YouTubeSearch.js
@@ -1,8 +1,9 @@
 import React from 'react';
 import { search } from '../../stories/8-Map-Components.stories';
 
+import { API_KEY } from '../../../youtubeApiKey.json';
+
 const YouTubeSearch = () => {
-	const API_KEY = 'AIzaSyACafPKPEEb1_7ppy7fN-Vv_28oszGoS9A';
 
 	function search(query) {
 		window.fetch(`https://www.googleapis.com/youtube/v3/search?access_token=${API_KEY}?part=${query}`);

94b9c31d4e108dbf9b0c5afff5c5163b9a461c41
ventenni <ventenni@gmail.com>
2021-03-17 17:53:44 +1000

    Removed directories OUT and PUBLIC from repo

--- webpack.config.js
+++ webpack.config.js
@@ -1,30 +1,33 @@
 const path = require('path');
 
-  module.exports = {
-    entry: './src/index.js',
-    output: {
-      filename: 'bundle.js',
-      path: path.resolve(__dirname, 'dist'),
-    },
-    module: {
-      rules: [
-        {
-          test: /\.css$/,
-          use: [
-            'style-loader',
-            'css-loader',
-          ],
-		},
-		{
-			test: /\.(png|svg|jpg|gif)$/,
-			use: [
-			'file-loader',
-			],
-    },
-    {
-      test: /\.(png|jpg)$/,
-      loader: 'url-loader'
-    }
-      ],
-    },
-  };
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: [
+          'style-loader',
+          'css-loader',
+        ],
+        options: {
+          url: true,
+        },
+      },
+  {
+    test: /\.(png|svg|jpg|gif)$/,
+    use: [
+    'file-loader',
+    ],
+  },
+  {
+    test: /\.(png|jpe?g|gif|svg|eot|ttf|woff|woff2)$/i,
+    loader: 'url-loader'
+  }
+    ],
+  },
+};

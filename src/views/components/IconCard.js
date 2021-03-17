7901f305a539c81a69709a7926b3daea29b7359c
ventenni <ventenni@gmail.com>
2021-03-17 21:29:14 +1000

    Icon card block is working

--- src/views/components/IconCard.js
+++ src/views/components/IconCard.js
@@ -5,17 +5,21 @@ import React from 'react';
 // Sass
 import './../../assets/styles/global.scss';
 
+// Icon
+import Icon from './../../assets/icons/accusoft.svg';
+
 const IconCard = ({ data }) => {
 
 	const heading = data.heading ? data.heading : "Heading Placeholder";
 	const copy = data.copy ? data.copy : "Elit cillum veniam aliquip do voluptate Lorem magna nostrud. Fugiat nostrud ipsum sint sint ad eu sunt irure. Ex officia labore";
-	const icon = data.icon ? data.icon : "500px";
+	// const icon = data.icon ? data.icon : "500px";
 
 	return (
 		<div className="vnt-icon-card">
 
 			<div className="vnt-icon-card__heading">
-				<i className={`vnt-icon vnt-icon__${icon}`}></i>
+				{/* <i className={`vnt-icon vnt-icon__${icon}`}></i> */}
+				<img className="vnt-icon" src={Icon} alt="500px Logo" />
 
 				<h3>{heading}</h3>
 			</div>
